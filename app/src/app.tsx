546d0238ea65c4b6cea93cb233e1b8547d12d7e3
nigimitama <nigimitama16@gmail.com>
2023-10-17 07:23:15 +0900

    improve layout

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -2,24 +2,38 @@ import { createRoot } from 'react-dom/client';
 import { useState, createContext } from 'react';
 import { DropArea } from './components/DropArea'
 import { DisplayInput } from './components/DisplayInput'
-
+import { Divider } from '@mui/material';
+import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications';
+import LanguageSelection from './components/LanguageSelection';
 export const appContext = createContext({})
+import Typography from '@mui/material/Typography';
 
 
 const App = () => {
   const [inputPaths, setInputPaths] = useState([])
+  const [languages, setLanguages] = useState([])
   const contextValues = {
     'inputPaths': inputPaths,
-    'setInputPaths': setInputPaths
+    'setInputPaths': setInputPaths,
+    'languages': languages,
+    'setLanguages': setLanguages
   }
 
   return (
     <appContext.Provider value={ contextValues }>
       <div>
-        <h2>Input</h2>
-        <DropArea />
-        <DisplayInput />
-        <footer className="footer"></footer>
+        <section>
+          <Typography variant="h6">Input</Typography>
+          <DropArea />
+          <DisplayInput />
+        </section>
+        <section style={{margin: '1em 0 1em 0'}}>
+          <Typography variant="h6" gutterBottom>
+            <SettingsApplicationsIcon/>
+            Settings
+          </Typography>
+          <LanguageSelection />
+        </section>
       </div>
     </appContext.Provider>
   )
